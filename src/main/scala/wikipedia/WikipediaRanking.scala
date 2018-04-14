d8ed2775746cfd95524f569e0d7df43b149224b1
Cisco Delgado <fdelgados@gmail.com>
2018-04-14 21:24:11 +0200

    Implement rankLangsReduceByKey

--- src/main/scala/wikipedia/WikipediaRanking.scala
+++ src/main/scala/wikipedia/WikipediaRanking.scala
@@ -78,7 +78,11 @@ object WikipediaRanking {
    *   Note: this operation is long-running. It can potentially run for
    *   several seconds.
    */
-  def rankLangsReduceByKey(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = ???
+  def rankLangsReduceByKey(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = {
+    rdd.flatMap(article => {
+      langs.filter(lang => article.mentionsLanguage(lang)).map(lang => (lang, 1))
+    }).reduceByKey(_ + _).collect().toList
+  }
 
   def main(args: Array[String]) {
 
